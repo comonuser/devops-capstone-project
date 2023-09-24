e10e73880b911f9d7b69b2784f638a8f1b63eb01
comonuser <144152633+comonuser@users.noreply.github.com>
2023-09-24 17:41:35 +0330

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -114,21 +114,18 @@ def update_accounts(account_id):
 # DELETE AN ACCOUNT
 ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
-def delete_account(account_id):
+def delete_accounts(account_id):
     """
     Delete an Account
-    This endpoint will delete an Account based on the id specified in the path
+    This endpoint will delete an Account based on the account_id that is requested
     """
-    app.logger.info("Request to delete Account with id [%s]", account_id)
-    account = Account.get(account_id)
+    app.logger.info("Request to delete an Account with id: %s", account_id)
+
+    account = Account.find(account_id)
     if account:
         account.delete()
-        app.logger.info("Account with id [%s] deleted", account_id)
-        return "", status.HTTP_204_NO_CONTENT
-    else:
-        app.logger.info("Account with id [%s] not found", account_id)
-        return "", status.HTTP_404_NOT_FOUND
 
+    return "", status.HTTP_204_NO_CONTENT
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
